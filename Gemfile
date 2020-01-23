8dfd50b98ead1a0ce113021ad53f43722b50da18
syamoji <36583417+syamoji56@users.noreply.github.com>
2020-01-23 14:31:00 +0900

    reporters　テストを色つけ表示を実行可能にした vscode上でデバッグを可能にした

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+gem "byebug"
+gem "cowsay", "~> 0.3.0"
+gem "debase"
+gem "minitest"
+gem "minitest-reporters"
+gem "ruby-debug-ide" # デバッグに必要
+gem "simplecov", require: false, group: :test
--- main.rb
+++ main.rb
@@ -1,13 +1,19 @@
-# require "minitest/reporters"
-# Minitest::Reporters.use!
+require "minitest/reporters"
+Minitest::Reporters.use!
 require "minitest/autorun"
 
-class HelloTest < Minitest::Test
-  def test_greeting
-    assert_equal "hello world", greeting
+class FizzBuzzTest < Minitest::Test
+    def test_1を渡したら文字列1を返す
+      assert_equal '1', FizzBuzz.generate(1)
+    end
+  
+    def test_2を渡したら文字列2を返す
+      assert_equal '2', FizzBuzz.generate(2)
+    end
   end
-end
 
-def greeting
-  "hello world"
-end
+  class FizzBuzz
+    def self.generate(n)
+      n.to_s
+    end
+  end
