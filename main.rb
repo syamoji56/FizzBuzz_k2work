19f38fc2fa62ea24ad88fd6cfff28f0ab4860838
syamoji <36583417+syamoji56@users.noreply.github.com>
2020-02-05 11:37:24 +0900

    refactor:メソッドの抽出

--- main.rb
+++ main.rb
@@ -1,19 +1,25 @@
-require "minitest/reporters"
+# frozen_string_literal: true
+
+require 'minitest/reporters'
 Minitest::Reporters.use!
-require "minitest/autorun"
+require 'minitest/autorun'
 
 class FizzBuzzTest < Minitest::Test
-    def test_1を渡したら文字列1を返す
-      assert_equal '1', FizzBuzz.generate(1)
-    end
+  def setup
+    @fizzbuzz=FizzBuzz
+  end
   
-    def test_2を渡したら文字列2を返す
-      assert_equal '2', FizzBuzz.generate(2)
-    end
+  def test_1を渡したら文字列1を返す
+    assert_equal '1', FizzBuzz.generate(1)
   end
 
-  class FizzBuzz
-    def self.generate(n)
-      n.to_s
-    end
+  def test_2を渡したら文字列2を返す
+    assert_equal '2', FizzBuzz.generate(2)
   end
+end
+
+class FizzBuzz
+  def self.generate(n)
+    n.to_s
+  end
+end
