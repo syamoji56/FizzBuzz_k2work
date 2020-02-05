4fd7a1e072d703b7207da70ce5041d17997e8f21
syamoji <36583417+syamoji56@users.noreply.github.com>
2020-02-05 11:46:45 +0900

    test: 3を渡したら文字列Fizzを返す

--- main.rb
+++ main.rb
@@ -16,10 +16,19 @@ class FizzBuzzTest < Minitest::Test
   def test_2を渡したら文字列2を返す
     assert_equal '2', FizzBuzz.generate(2)
   end
+
+  def test_3を渡したら文字列Fizzを返す
+    assert_equal 'fizz',@fizzbuzz.generate(3)
+  end
+
 end
 
 class FizzBuzz
   def self.generate(number)
-    number.to_s
+    result=number.to_s
+    if number%3==0
+      result='fizz'
+    end
+    result
   end
 end
