35fdd63793db5aa8822ed1ecf504e4c475ac0ec1
syamoji <36583417+syamoji56@users.noreply.github.com>
2020-02-09 22:10:01 +0900

    test: タイプ２は数字のみ　3.5.15のテスト

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -44,14 +44,35 @@ class FizzBuzzTest < Minitest::Test
     end
 
     describe 'タイプ2の場合' do
- 
+      def setup
+        @type = 2        
+      end
+
+      describe '３の倍数の場合' do
+        def test_3を渡したら文字列3を返す
+          assert_equal '3', FizzBuzz.generate(3, @type)
+        end
+      end
+
+      describe '５の倍数の場合' do
+        def test_5を渡したら文字列5を返す
+          assert_equal '5', FizzBuzz.generate(5, @type)
+        end
+      end
+
+      describe '３と５の倍数の場合' do
+        def test_15を渡したら文字列15を返す
+          assert_equal '15', FizzBuzz.generate(15, @type)
+        end
+      end
+
       describe 'その他の場合' do
         def test_1を渡したら文字列1を返す
-          assert_equal '1', FizzBuzz.generate(1, 2)
+          assert_equal '1', FizzBuzz.generate(1, @type)
         end
 
         def test_2を渡したら文字列2を返す
-          assert_equal '2', FizzBuzz.generate(2, 2)
+          assert_equal '2', FizzBuzz.generate(2, @type)
         end
       end
     end
