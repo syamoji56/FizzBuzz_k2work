fea3b6efd91f4ec193d209b05f86b553b8ad0c47
syamoji <36583417+syamoji56@users.noreply.github.com>
2020-02-09 21:33:50 +0900

    test: タイプ１は通常

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -4,19 +4,21 @@
 class FizzBuzz
   MAX_NUMBER = 100
 
-  def self.generate(number)
-    is_fizz = number.modulo(3).zero?
-    is_buzz = number.modulo(5).zero?
+  def self.generate(number, type)
+    if type == 1
+      is_fizz = number.modulo(3).zero?
+      is_buzz = number.modulo(5).zero?
 
-    return 'FizzBuzz' if is_fizz && is_buzz
-    return 'Fizz' if is_fizz
-    return 'Buzz' if is_buzz
+      return 'FizzBuzz' if is_fizz && is_buzz
+      return 'Fizz' if is_fizz
+      return 'Buzz' if is_buzz
 
-    number.to_s
+      number.to_s
+    end
   end
 
   def self.generate_list
     # 1から最大値までのFizzBuzz配列を１発で作る
-    (1..MAX_NUMBER).map { |n| generate(n) }
+    (1..MAX_NUMBER).map { |n| generate(n, 1) }
   end
 end
--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -13,31 +13,33 @@ class FizzBuzzTest < Minitest::Test
       @fizzbuzz = FizzBuzz
     end
 
-    describe '３の倍数の場合' do
-      def test_3を渡したら文字列Fizzを返す
-        assert_equal 'Fizz', @fizzbuzz.generate(3)
-      end
-    end
-
-    describe '５の倍数の場合' do
-      def test_5を渡したら文字列Buzzを返す
-        assert_equal 'Buzz', @fizzbuzz.generate(5)
-      end
-    end
-
-    describe '３と５の倍数の場合' do
-      def test_15を渡したら文字列FizzBuzzを返す
-        assert_equal 'FizzBuzz', @fizzbuzz.generate(15)
-      end
-    end
-
-    describe 'その他の場合' do
-      def test_1を渡したら文字列1を返す
-        assert_equal '1', FizzBuzz.generate(1)
+    describe 'タイプ１の場合' do
+      describe '３の倍数の場合' do
+        def test_3を渡したら文字列Fizzを返す
+          assert_equal 'Fizz', @fizzbuzz.generate(3, 1)
+        end
       end
 
-      def test_2を渡したら文字列2を返す
-        assert_equal '2', FizzBuzz.generate(2)
+      describe '５の倍数の場合' do
+        def test_5を渡したら文字列Buzzを返す
+          assert_equal 'Buzz', @fizzbuzz.generate(5, 1)
+        end
+      end
+
+      describe '３と５の倍数の場合' do
+        def test_15を渡したら文字列FizzBuzzを返す
+          assert_equal 'FizzBuzz', @fizzbuzz.generate(15, 1)
+        end
+      end
+
+      describe 'その他の場合' do
+        def test_1を渡したら文字列1を返す
+          assert_equal '1', FizzBuzz.generate(1, 1)
+        end
+
+        def test_2を渡したら文字列2を返す
+          assert_equal '2', FizzBuzz.generate(2, 1)
+        end
       end
     end
 
