765f2b02a9336ba335c865db39815a7fadde71dc
syamoji <36583417+syamoji56@users.noreply.github.com>
2020-02-09 22:30:17 +0900

    refactor: タイプ１とタイプ２のsetupメソッド

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -9,43 +9,44 @@ require './lib/fizz_buzz'
 
 class FizzBuzzTest < Minitest::Test
   describe 'FizzBuzz' do
-    def setup
-      @fizzbuzz = FizzBuzz
-    end
+    describe 'タイプ１は通常' do
+      def setup
+        @fizzbuzz = FizzBuzz
+        @type = 1
+      end
 
-    describe 'タイプ１の場合' do
       describe '３の倍数の場合' do
         def test_3を渡したら文字列Fizzを返す
-          assert_equal 'Fizz', @fizzbuzz.generate(3, 1)
+          assert_equal 'Fizz', @fizzbuzz.generate(3, @type)
         end
       end
 
       describe '５の倍数の場合' do
         def test_5を渡したら文字列Buzzを返す
-          assert_equal 'Buzz', @fizzbuzz.generate(5, 1)
+          assert_equal 'Buzz', @fizzbuzz.generate(5, @type)
         end
       end
 
       describe '３と５の倍数の場合' do
         def test_15を渡したら文字列FizzBuzzを返す
-          assert_equal 'FizzBuzz', @fizzbuzz.generate(15, 1)
+          assert_equal 'FizzBuzz', @fizzbuzz.generate(15, @type)
         end
       end
 
       describe 'その他の場合' do
         def test_1を渡したら文字列1を返す
-          assert_equal '1', @fizzbuzz.generate(1, 1)
+          assert_equal '1', @fizzbuzz.generate(1, @type)
         end
 
         def test_2を渡したら文字列2を返す
-          assert_equal '2', @fizzbuzz.generate(2, 1)
+          assert_equal '2', @fizzbuzz.generate(2, @type)
         end
       end
     end
 
-    describe 'タイプ2の場合' do
+    describe 'タイプ2は数字のみ' do
       def setup
-        @fizzbuzz = FizzBuzz # これないとno method errorになる
+        @fizzbuzz = FizzBuzz
         @type = 2        
       end
 
